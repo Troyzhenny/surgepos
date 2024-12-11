8662373d42a4e25baead8479a78aa1d26d123d52
Tevin Campbell <tevincampbell73@yahoo.com>
2024-12-11 02:47:16 -0500

    added prop type for button

--- client/src/components/ui/PrimaryButton.jsx
+++ client/src/components/ui/PrimaryButton.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 
-export function PrimaryButton({ label }) {
+export function PrimaryButton({ label, onClick }) {
   return (
-    <button className="w-80 h-12 bg-dark rounded-sm text-white font-semibold flex items-center justify-center transition-all hover:bg-transparent hover:text-dark hover:border-2 border-dark">
+    <button type="button" onClick={onClick} className="w-80 h-12 bg-dark rounded-sm text-white font-semibold flex items-center justify-center transition-all hover:bg-transparent hover:text-dark hover:border-2 border-dark">
       {label}
     </button>
   );
@@ -10,4 +10,5 @@ export function PrimaryButton({ label }) {
 
 PrimaryButton.propTypes = {
   label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
 };
