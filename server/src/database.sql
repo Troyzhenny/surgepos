226cb0babcd9136d9408fd4010d6989617b44e8f
Tevin Campbell <tevincampbell73@yahoo.com>
2024-12-12 15:56:20 -0500

    added: tables for admin and employees with new table data

---
+++ server/src/database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE surgepos;
+
+CREATE TABLE employees(
+    employee_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_demo BOOLEAN DEFAULT FALSE,
+    description VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE admins(
+    admin_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_demo BOOLEAN DEFAULT FALSE,
+    description VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
