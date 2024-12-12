e0a594215535d49073125a2ad9a25883c593a2b2
Tevin Campbell <tevincampbell73@yahoo.com>
2024-12-12 15:55:37 -0500

    imported middlewares  anst begin route setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-
+const pool = require('./src/config/db');
+const cors = require('cors');
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+
 const PORT = 8000;
 
-// Endpoints
-app.get('/', (req, res) => {
-    console.log('endpoint num1 hit', req.method)
-    res.sendStatus(201)
-});
+// Middleware
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+
+//_____ Routes _____ //
+
+
 
 app.listen(PORT, () => console.log(`server has started on ${PORT}`));
