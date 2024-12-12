4cd10cf249d0c0ea96ac4014c7c266548ce66a31
Tevin Campbell <tevincampbell73@yahoo.com>
2024-12-12 01:33:54 -0500

    added: constants for admin navigation

---
+++ client/src/constants/index.js
@@ -0,0 +1,9 @@
+const navList = [
+    { name: "Home", path: "/admin-panel" },
+    { name: "Employees", path: "#" },
+    { name: "Schedule", path: "#" },
+    { name: "Support", path: "#" }
+];
+
+
+export default navList;
