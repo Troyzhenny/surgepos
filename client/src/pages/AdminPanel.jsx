3055321aa83a4e5885ee40559195ff885e9bfdb5
Tevin Campbell <tevincampbell73@yahoo.com>
2024-12-12 01:33:36 -0500

    layout for admin page

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -1,7 +1,35 @@
+import { Cards } from "../components/ui/Cards";
+import navList from "../constants";
+import { Link } from "react-router-dom";
+import { LuCircleUserRound } from "react-icons/lu";
+
+
 export function AdminPanel() {
-    return(
-        <div className="w-full h-screen">
-            <h1 className="text-2xl text-dark">Admin Page</h1>
+  return (
+    <div className="w-full h-screen bg-dark flex flex-col items-center justify-center">
+      <header className="w-full h-12 flex items-center justify-between">
+        <img src="/assets/images/surge-w.svg" alt="surge logo" role="logo" />
+        <nav className="text-white">
+          <ul className="flex gap-10">
+            {navList.map((nav) => (
+              <Link key={nav.name} to={nav.path} className="hover:text-green-500 transition-all">
+                {nav.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+        <div className="flex items-center gap-2">
+            <LuCircleUserRound className="text-white text-3xl"/>
+            <select name="" id="">
+                <option>foo</option>
+                <option>bar</option>
+                <option>baz</option>
+            </select>
         </div>
-    )
+      </header>
+      <section id="content" className="flex flex-col w-full h-full items-center justify-center">
+        <Cards />
+      </section>
+    </div>
+  );
 }
