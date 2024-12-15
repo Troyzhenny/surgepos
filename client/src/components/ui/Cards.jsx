c4c8b1113c53dc59e910d81a095ab8f39fcf761c
Tevin Campbell <tevincampbell73@yahoo.com>
2024-12-15 08:12:45 -0500

    made cards more flexible

--- client/src/components/ui/Cards.jsx
+++ client/src/components/ui/Cards.jsx
@@ -1,5 +1,15 @@
-export function Cards() {
+import PropTypes from "prop-types"
+
+export function Cards({ label, className }) {
     return(
-        <div className="w-40 h-40 rounded-md bg-gray-200"></div>
+        <div className={`${className} w-40 h-40 border`}>
+            {label}
+        </div>
     )
+}
+
+
+Cards.propTypes = {
+    label: PropTypes.string.isRequired,
+    className: PropTypes.node
 }
