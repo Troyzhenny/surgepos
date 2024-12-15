ac960484c7e6b719b6fdc80b09136ae2e3931e28
Tevin Campbell <tevincampbell73@yahoo.com>
2024-12-15 08:12:26 -0500

    rm: navlist, used cards instead

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,9 +1,9 @@
-const navList = [
-    { name: "Home", path: "/admin-panel" },
-    { name: "Employees", path: "#" },
-    { name: "Schedule", path: "#" },
-    { name: "Support", path: "#" }
+const AdminCards = [
+    { id:1, name: "Time Punch", path: "#" },
+    { id:2, name: "Employees", path: "#" },
+    { id:3, name: "Schedule", path: "#" },
+    { id:4, name: "Support", path: "#" }
 ];
 
 
-export default navList;
+export default AdminCards;
