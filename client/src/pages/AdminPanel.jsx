4b641cdf713f8f37c50aa31697aca49f0d0d056a
Troyzhenny <75921907+Troyzhenny@users.noreply.github.com>
2025-01-29 17:47:44 -0500

    Update layout of admin panel

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -1,34 +1,69 @@
 import { Cards } from "../components/ui/Cards";
-import navList from "../constants";
+import AdminCards from "../constants";
 import { Link } from "react-router-dom";
 import { LuCircleUserRound } from "react-icons/lu";
-
+import surgeIcon from "../assets/images/surge-w.svg";
+import { useState } from "react";
 
 export function AdminPanel() {
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  const handleToggleDropdown = () => {
+    setToggleDropdown((prevState) => !prevState);
+  };
+
   return (
-    <div className="w-full h-screen bg-dark flex flex-col items-center justify-center">
-      <header className="w-full h-12 flex items-center justify-between">
-        <img src="/assets/images/surge-w.svg" alt="surge logo" role="logo" />
-        <nav className="text-white">
-          <ul className="flex gap-10">
-            {navList.map((nav) => (
-              <Link key={nav.name} to={nav.path} className="hover:text-green-500 transition-all">
-                {nav.name}
-              </Link>
-            ))}
-          </ul>
-        </nav>
+    <div className="w-full h-screen bg-dark bg-gradient-to-t from-dark flex flex-col items-center justify-center">
+      <header className="w-full h-12 flex items-center justify-between p-10">
+        <Link to="/admin-panel">
+          <img
+            src={surgeIcon}
+            alt="surge logo"
+            role="logo"
+            className="w-20 h-20"
+          />
+        </Link>
         <div className="flex items-center gap-2">
-            <LuCircleUserRound className="text-white text-3xl"/>
-            <select name="" id="">
-                <option>foo</option>
-                <option>bar</option>
-                <option>baz</option>
-            </select>
+          <p className="text-white">admin.name</p>
+          <LuCircleUserRound
+            className="text-white text-3xl cursor-pointer"
+            onClick={handleToggleDropdown}
+          />
+          {toggleDropdown && (
+            <div
+              role="dropdown menu"
+              className="text-white absolute flex flex-col bg-zinc-800 w-40 top-[81px] p-5 transition-all"
+            >
+              <Link
+                className="hover:text-green-500 active:text-orange-300 focus:text-orange-300"
+                onClick={""}
+              >
+                Logout
+              </Link>
+              <Link className="hover:text-green-500 active:text-orange-300 focus:text-orange-300">
+                Settings
+              </Link>
+              <Link className="hover:text-green-500 active:text-orange-300 focus:text-orange-300">
+                Support
+              </Link>
+            </div>
+          )}
         </div>
       </header>
-      <section id="content" className="flex flex-col w-full h-full items-center justify-center">
-        <Cards />
+      <section
+        id="content"
+        className="flex flex-col w-full h-full items-center justify-center"
+      >
+        <div className="flex flex-wrap items-center justify-center w-fit h-fit gap-10">
+          {AdminCards.map((data) => (
+            <Link key={data.id} to={data.path}>
+              <Cards
+                label={data.name}
+                className="flex items-center justify-center rounded-lg text-orange-100 text-lg bg-green-600 bg-gradient-to-t from-dark bg-opacity-5 border-2 w-72 h-44 border-sage transition-all hover:translate hover:bg-opacity-10 focus:bg-opacity-10 active:bg-opacity-10"
+              />
+            </Link>
+          ))}
+        </div>
       </section>
     </div>
   );
