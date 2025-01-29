4db93e86e8413c4229545e503d95f287a4899251
Troyzhenny <75921907+Troyzhenny@users.noreply.github.com>
2025-01-29 17:51:01 -0500

    Update: rename file

--- client/src/constants/index.js
+++
@@ -1,9 +0,0 @@
-const AdminCards = [
-    { id:1, name: "Time Punch", path: "#" },
-    { id:2, name: "Employees", path: "#" },
-    { id:3, name: "Schedule", path: "#" },
-    { id:4, name: "Support", path: "#" }
-];
-
-
-export default AdminCards;
---
+++ client/src/utils/constants.js
@@ -0,0 +1,13 @@
+const AdminCards = [
+    { id:1, name: "Time Punch", path: "#" },
+    { id:2, name: "Employees", path: "#" },
+    { id:3, name: "Scheduler", path: "#" },
+    { id:4, name: "Back Office", path: "#" },
+    { id:8, name: "Order Portal", path: "#" },
+    { id:6, name: "Reviews", path: "#" },
+    { id:7, name: "Loyalty Program", path: "#" },
+    { id:5, name: "Reports", path: "#" },
+];
+
+
+export default AdminCards;
